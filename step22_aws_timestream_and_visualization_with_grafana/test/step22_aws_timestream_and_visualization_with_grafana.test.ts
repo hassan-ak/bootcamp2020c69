15d14f8c9f4926184125ad302daa45035b9dca54
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-26 12:41:59 +0500

    cdk app

---
+++ step22_aws_timestream_and_visualization_with_grafana/test/step22_aws_timestream_and_visualization_with_grafana.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step22AwsTimestreamAndVisualizationWithGrafana from '../lib/step22_aws_timestream_and_visualization_with_grafana-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step22_aws_timestream_and_visualization_with_grafana-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step22AwsTimestreamAndVisualizationWithGrafana.Step22AwsTimestreamAndVisualizationWithGrafanaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
